b6dd77e8a8738f21dfd664ff81d28efef88be061
ANKRITIS <128342178+ANKRITIS@users.noreply.github.com>
2025-01-29 18:51:08 -0700

    Update Circle.java
    Added color field to Circle

--- code/app/src/main/java/com/example/code/Circle.java
+++ code/app/src/main/java/com/example/code/Circle.java
@@ -2,13 +2,18 @@ package com.example.code;
 
 public class Circle extends Shape {
     private int radius;
+    
 
     public Circle(int x, int y, int radius) {
         super(x, y);
         this.radius = radius;
+       this.color = color;
+
+        
     }
 
     public int getRadius() {
         return radius;
     }
+ protected String color = "red";
 }
